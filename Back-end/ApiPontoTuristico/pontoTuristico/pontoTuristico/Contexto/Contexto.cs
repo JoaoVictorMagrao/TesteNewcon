3be91f235c8019618afcb57b26759a87d425dfd1
João Victor Magrão <joaovmagrao10@gmail.com>
2022-12-22 08:55:24 -0300

    Projeto Final
    Projeto Ponto Turístico

---
+++ Back-end/ApiPontoTuristico/pontoTuristico/pontoTuristico/Contexto/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pontoTuristico.Models;
+
+namespace pontoTuristico.Contexto
+{
+    public class Contexto : DbContext
+    {
+        
+
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) => Database.EnsureCreated();
+
+        public DbSet<cPontoTuristico> cPontoTuristico { get; set; }
+    }
+}
---
+++ Back-end/ApiPontoTuristico/pontoTuristico/pontoTuristico/Models/cPontoTuristico.cs
@@ -0,0 +1,17 @@
+﻿namespace pontoTuristico.Models
+{
+    public class cPontoTuristico
+    {
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public string? endereco { get; set; }
+
+        public string? Estado { get; set; }
+
+        public string? Cidade { get; set; }
+    }
+}
